cdfb3dde5f0ce44d91b738394c180f064ea0fa8d
EltonCampos07 <eltonapcnunes@gmail.com>
2024-05-11 19:26:13 -0300

    chore: configuração inicial

---
+++ fabrica_automoveis/src/main/java/br/com/up/fabrica_automoveis/FabricaAutomoveisApplication.java
@@ -0,0 +1,13 @@
+package br.com.up.fabrica_automoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaAutomoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaAutomoveisApplication.class, args);
+	}
+
+}
---
+++ fabrica_automoveis/src/test/java/br/com/up/fabrica_automoveis/FabricaAutomoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.up.fabrica_automoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaAutomoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
